3ebc860001a1a40aada4b680dbb5e3ad012aa8c9
DGtalize <admin@dgtalize.com>
2015-11-08 22:52:59 -0300

    Ignore vendor directory

--- Symfony/src/Pimx/ApiBundle/Controller/AccountController.php
+++ Symfony/src/Pimx/ApiBundle/Controller/AccountController.php
@@ -2,20 +2,24 @@
 
 namespace Pimx\ApiBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-
 use FOS\RestBundle\Controller\Annotations;
-use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations\ApiDoc;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations\RouteResource;
+use FOS\RestBundle\Routing\ClassResourceInterface;
 
 use Pimx\ModelBundle\Entity\Account;
 
-class AccountController extends FOSRestController {
+/**
+ * @RouteResource("Account", pluralize=false)
+ */
+class AccountController extends Controller /*FOSRestController*/ implements ClassResourceInterface {
 
-    /**
-     * @Rest\View
-     */
-    public function allAction() {
+    public function cgetAction() {
         $accounts = $this->getDoctrine()
                 ->getRepository('PimxModelBundle:Account')
                 ->findAll()
@@ -24,9 +28,6 @@ class AccountController extends FOSRestController {
         return array('accounts' => $accounts);
     }
 
-    /**
-     * @Rest\View
-     */
     public function getAction($code) {
         $account = $this->getDoctrine()
                 ->getRepository('PimxModelBundle:Account')
@@ -40,4 +41,7 @@ class AccountController extends FOSRestController {
         return array('account' => $account);
     }
 
+    public function postAction(Request $request) {
+        return array('result' => 'OK');
+    }
 }
