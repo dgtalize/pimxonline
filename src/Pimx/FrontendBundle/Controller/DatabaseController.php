b457b563a65422c52b025efc80f816d101146efe
dgtalize <dgForge11>
2016-03-26 23:10:54 -0300

    Backup database

--- src/Pimx/FrontendBundle/Controller/DatabaseController.php
+++ src/Pimx/FrontendBundle/Controller/DatabaseController.php
@@ -6,29 +6,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Pimx\DoctrineBundle\Connection\ConnectionWrapper;
 
 class DatabaseController extends Controller {
 
     public function backupAction(Request $request) {
+        $logger = $this->get('logger');
+
         /* @var $session Symfony\Component\HttpFoundation\Session\Session */
         $session = $this->get('session');
         $dbParams = $session->get(ConnectionWrapper::SESSION_ACTIVE_DYNAMIC_CONN);
-        $user = $dbParams['user'];
-        $password = $dbParams['password'];
+        $user = $dbParams['dbuser'];
+        $password = $dbParams['dbpass'];
         $database = $dbParams['dbname'];
-        
-        $backupFile = $this->get('kernel')->getRootdir().'/../public_html/db/backup.sql.gz';
-        
-        $command = "mysqldump -u $user -p'$password' $database | gzip > $backupFile";
 
-        $result = system($command);
-        
-        $response = new Response();
-        $response->setContent($result);
-        
-        return $response;
+        $backupDirPath = $this->get('kernel')->getRootdir() . '/../db/';
+        $backupFileName = $database . " " . date('Y-m-d') . ".sql";
+        $backupFilePath = $backupDirPath . $backupFileName;
+
+        $command = "mysqldump -u $user -p'$password' $database --complete-insert --result-file=\"$backupFilePath\"";
+//        $command = "mysqldump -u $user -p'$password' $database";
+        $logger->info("Database backup: $command");
+
+        try {
+            $result = system($command);
+            
+            $response = new BinaryFileResponse($backupFilePath);
+            $response->headers->set('Content-Type', 'text/plain');
+            $response->setContentDisposition(
+                    ResponseHeaderBag::DISPOSITION_ATTACHMENT, $backupFileName
+            );
+
+            return $response;
+        } catch (\Exception $ex) {
+            $logger->info("Database backup error: " . $ex->getMessage());
+            $translator = $this->get('translator');
+            $this->addFlash('danger', $translator->trans('database.backup_error'));
+
+            return $this->render('PimxFrontendBundle:Database:backup.html.twig', array());
+        }
     }
 
 }
