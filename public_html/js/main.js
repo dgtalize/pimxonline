4f4b6cb03c257baacdc74999a0b47bde83a2ba5e
DGtalize <admin@dgtalize.com>
2017-12-09 19:01:42 -0300

    select2 integration ui tweaks

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,8 @@
-$(document).ready(function() {
-	$('.btn-delete').click(function(){
-		return confirm('Are you sure you want to delete this element?');
-	});
+$(document).ready(function () {
+    $('.btn-delete').click(function () {
+        return confirm('Are you sure you want to delete this element?');
+    });
+
+    $.fn.select2.defaults.set("theme", "bootstrap");
+    $('.select2').select2();
 });
--- src/Pimx/FrontendBundle/Form/Type/MovementAccountType.php
+++ src/Pimx/FrontendBundle/Form/Type/MovementAccountType.php
@@ -2,35 +2,44 @@
 
 namespace Pimx\FrontendBundle\Form\Type;
 
+use Doctrine\ORM\EntityRepository;
+use Pimx\ModelBundle\Entity\Account;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class MovementAccountType extends AbstractType {
+class MovementAccountType extends AbstractType
+{
 
-    public function buildForm(FormBuilderInterface $builder, array $options) {
-        $builder->add('account', 'entity', array(
-                    'class' => 'PimxModelBundle:Account',
-                    'property' => 'description',
-                ))
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('account', EntityType::class, array(
+            'class' => Account::class,
+            'property' => 'description',
+            'query_builder' => function (EntityRepository $repository) {
+                return $repository->createQueryBuilder('a')->orderBy('a.description', 'ASC');
+            }
+        ))
 //                ->add('sign', 'choice', array(
 //                    'choices' => array('1' => 'In', '-1' => 'Out'),
 //                    'required' => true
 //                ))
-                ->add('sign', HiddenType::class, array(
-                    'required' => true
-                ))
-                ->add('amount', NumberType::class)
-            ;
+            ->add('sign', HiddenType::class, array(
+                'required' => true
+            ))
+            ->add('amount', NumberType::class);
     }
 
-    public function getName() {
+    public function getName()
+    {
         return 'movementaccount';
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
         $resolver->setDefaults(array(
             'data_class' => 'Pimx\ModelBundle\Entity\MovementAccount',
         ));
